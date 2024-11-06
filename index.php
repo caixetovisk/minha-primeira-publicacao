3c588af3357ecf6f224730116c6a847f21f29152
caixetovisk <58188092+caixetovisk@users.noreply.github.com>
2024-11-06 20:38:42 -0300

    teste

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php
+// Conexão com o banco de dados
+$conn = new mysqli("localhost", "usuario", "senha", "banco_de_dados");
 
-$senha = md5($_POST['password']);
+// Verifica a conexão
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
 
-echo $senha;
+// Obtém o nome de usuário da solicitação
+$username = $_GET['username'];
+
+// Consulta SQL vulnerável
+$sql = "SELECT * FROM usuarios WHERE username = '$username'";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "ID: " . $row["id"]. " - Nome: " . $row["nome"]. "<br>";
+    }
+} else {
+    echo "0 resultados";
+}
+
+$conn->close();
+?>
